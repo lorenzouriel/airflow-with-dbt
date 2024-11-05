8b09898a1f24bbe620912bb6619a2c6c192044d6
Lorenzo Uriel <lorenzouriel394@gmail.com>
2024-11-04 22:29:49 -0300

    MVC Code finished

--- dags/api_to_postgres.py
+++ dags/api_to_postgres.py
@@ -1,6 +1,5 @@
 from airflow.decorators import task, dag
-from include.controller import gerar_numero_aleatorio, fetch_pokemon_data, add_pokemon_to_db
-
+from include.controller import fetch_pokemon_data, add_pokemon_to_db, gerar_numero_aleatorio
 from datetime import datetime
 
 @dag(dag_id="capturar_pokemon",
@@ -15,13 +14,13 @@ def capturar_pokemon():
         return gerar_numero_aleatorio()
 
     @task(task_id='fetch_pokemon_data')
-    def task_fetch_pokemon_data():
-        return fetch_pokemon_data()
-
+    def task_fetch_pokemon_data(numero_aleatorio):
+        return fetch_pokemon_data(numero_aleatorio)
+    
     @task(task_id='add_pokemon_to_db')
-    def task_add_pokemon_to_db():
-        return add_pokemon_to_db()
-
+    def task_add_pokemon_to_db(pokemon_data):
+        add_pokemon_to_db(pokemon_data)
+    
     t1 = task_gerar_numero_aleatorio()
     t2 = task_fetch_pokemon_data(t1)
     t3 = task_add_pokemon_to_db(t2)
